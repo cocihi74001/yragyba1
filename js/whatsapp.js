9916cfb908b0b294b1136ff26c0582c6a1df3c9a
cocihi74001 <132769676+cocihi74001@users.noreply.github.com>
2023-10-29 16:11:15 +0300

    Add files via upload

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -13,16 +13,10 @@ $(function() {
 
     // make the ?text= payload, 
     // - could use .filter() here too if you dont want empty values
-    text = [
-      data.people_count,
-      data.email,
-      data.wpp,
-      data.cep,
-      data.rua,
-      data.bairro,
-      data.complemento
-    ].join(' - ') // change to what you want sep to be
-    text = 'Здраствуйте, хотели бы забронироться на тур. Нас ${people_count}'
+      
+    text = `Здраствуйте, хотели бы забронироться на тур. Нас ${data.people_count} человек.\nДата заезда - ${data.startDate}, дата выезда - ${data.endDate}.\nДней на катере - ${data.boat_count}. Количество электроспинингов - ${data.rod_count}. Часов морской прогулки - ${data.sea_hours_count}. ${document.querySelector('#island_check').checked ? "Тур до острова. " : ""}
+    ${document.querySelector('#boat_check').checked ? "Доставка до катера. " : ""} ${document.querySelector('#eda_check').checked ? "Обед-ужин. " : ""}
+    ${document.querySelector('#sauna_check').checked ? "Сауна. " : ""} ${document.querySelector('#aero_but').checked ? "Трансфер с аэропорта. " : "Трансфер с жд вокзала или с города"} Примерная стоимость тура - ${days_between(data.startDate, data.endDate) * (document.querySelector('#sauna_check').checked ? 12000: 8000) + data.rod_count*2500 + data.sea_hours_count*2500 + (data.people_count>4?3000:6000) + (data.boat_count*30000) + (document.querySelector('#boat_check').checked ? 500: 0)} рублей`
     // make the url
     const action = "https://wa.me/" + phone + "?text=" + encodeURIComponent(text);
     console.log(action)
@@ -30,3 +24,15 @@ $(function() {
 	 window.open(action, "_blank");;
   });
 });
+function days_between(date1, date2) {
+
+    // The number of milliseconds in one day
+    const ONE_DAY = 1000 * 60 * 60 * 24;
+
+    // Calculate the difference in milliseconds
+    const differenceMs = Math.abs(new Date(date1) - new Date(date2));
+
+    // Convert back to days and return
+    return Math.round(differenceMs / ONE_DAY);
+
+}
